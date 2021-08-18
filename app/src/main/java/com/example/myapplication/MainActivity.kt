c9fc4a50e6635ff8d40eaa71311fc9c04536670c
feyzagezerer <feyza.gezerer@ambeent.ai>
2021-08-18 08:39:23 -0700

    added alert dialog

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,6 +5,7 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
@@ -22,6 +23,7 @@ import androidx.core.content.ContextCompat
 class MainActivity : AppCompatActivity() {
     private val CHANNEL_ID = "channel_id"
     private val notificationId = 101
+    private val notificationId2 = 102
 
     val FINE_LOCATION_RQ = 101
     val CAMERA_RQ = 102
@@ -33,23 +35,36 @@ class MainActivity : AppCompatActivity() {
         // Timber.e( "i2n CALISIYO")
         createNotificationChannel()
 
+
+        buttonTaps()
+
+
+    }
+
+    private fun buttonTaps() {
+        val btnCamera = findViewById(R.id.btnCamera) as Button
+        btnCamera.setOnClickListener {
+            checkForPermissions(android.Manifest.permission.CAMERA, "camera", CAMERA_RQ)
+        }
+        val btnLocation = findViewById(R.id.btnLocation) as Button
+        btnLocation.setOnClickListener {
+            checkForPermissions(
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                "location",
+                FINE_LOCATION_RQ
+            )
+        }
         val btnNoti = findViewById(R.id.btnNoti) as Button
         btnNoti.setOnClickListener {
             sendNotification()
         }
-        buttonTaps()
-    }
-    private fun buttonTaps(){
-    val btnCamera = findViewById(R.id.btnCamera) as Button
-        btnCamera.setOnClickListener{
-            checkForPermissions(android.Manifest.permission.CAMERA,"camera",CAMERA_RQ)
-        }
-        val btnLocation = findViewById(R.id.btnLocation) as Button
-        btnLocation.setOnClickListener{
-            checkForPermissions(android.Manifest.permission.ACCESS_FINE_LOCATION,"location",FINE_LOCATION_RQ)
+        val btnAlertDialog = findViewById(R.id.btnAlertDialog) as Button
+        btnAlertDialog.setOnClickListener {
+            alertDialog()
         }
 
     }
+
     private fun createNotificationChannel() {
         // Timber.e(       "notification CALISIYO")
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -63,6 +78,17 @@ class MainActivity : AppCompatActivity() {
                 getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
         }
+    }
+
+    private fun alertDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Permission")
+        builder.setMessage("Do you allow?")
+        builder.setPositiveButton("Yes", { dialogInterface: DialogInterface, i: Int ->
+            finish()
+        })
+        builder.setNegativeButton("No", { dialogInterface: DialogInterface, i: Int -> })
+        builder.show()
     }
 
     private fun sendNotification() {
@@ -115,33 +141,44 @@ class MainActivity : AppCompatActivity() {
                     name,
                     requestCode
                 )
-                else-> ActivityCompat.requestPermissions(this,arrayOf(permission),requestCode)
+                else -> ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
             }
 
         }
 
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-      fun innerCheck(name: String){
-          if(grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED){
-              Toast.makeText(applicationContext,"$name permission refused",Toast.LENGTH_SHORT).show()
-          }else{
-              Toast.makeText(applicationContext,"$name permission granted",Toast.LENGTH_SHORT).show()
-          }
-      }
-        when(requestCode){
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        fun innerCheck(name: String) {
+            if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(applicationContext, "$name permission refused", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                Toast.makeText(applicationContext, "$name permission granted", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+        when (requestCode) {
             FINE_LOCATION_RQ -> innerCheck("location")
             CAMERA_RQ -> innerCheck("camera")
         }
     }
-    private fun showDialog(permission: String,name:String,requestCode:Int){
+
+    private fun showDialog(permission: String, name: String, requestCode: Int) {
         val builder = AlertDialog.Builder(this)
         builder.apply {
             setMessage("Permission to access your $name is requred to use this app")
             setTitle("Permission required")
-            setPositiveButton("OK"){dialog,which->
-                ActivityCompat.requestPermissions(this@MainActivity, arrayOf(permission),requestCode)
+            setPositiveButton("OK") { dialog, which ->
+                ActivityCompat.requestPermissions(
+                    this@MainActivity,
+                    arrayOf(permission),
+                    requestCode
+                )
             }
         }
         val dialog = builder.create()
