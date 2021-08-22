bcf31d4c1154a692ea955f1acf690ae494c6932b
feyzagezerer <feyza.gezerer@ambeent.ai>
2021-08-22 09:40:17 -0700

    reply and two action button options

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,233 +1,106 @@
 package com.example.myapplication
-
-import android.annotation.SuppressLint
-import android.app.*
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.ClipDescription
 import android.content.Context
-import android.content.DialogInterface
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.BitmapFactory
-import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.RemoteViews
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import androidx.core.content.ContextCompat
-
+import androidx.core.app.RemoteInput
+import androidx.core.content.getSystemService
+//import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private val CHANNEL_ID = "channel_id"
-    private val notificationId = 101
-    private val notificationId2 = 102
-
-    val FINE_LOCATION_RQ = 101
-    val CAMERA_RQ = 102
-
-    lateinit var notificationManager: NotificationManager
-    lateinit var notificationChannel: NotificationChannel
-    lateinit var builder: NotificationCompat.Builder
-
-    private val channelId = "com.example.myapplication"
-    private val descriptor = "Fey notifi"
+    private val channelID = "com.example.myapplication.channel1"
+    private var notificationManager: NotificationManager? = null
+    private val KEY_REPLY = "key_reply"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        buttonTaps()
-    }
-
-    private fun buttonTaps() {
-        val btnCamera = findViewById(R.id.btnCamera) as Button
-        btnCamera.setOnClickListener {
-            checkForPermissions(android.Manifest.permission.CAMERA, "camera", CAMERA_RQ)
-        }
-        val btnLocation = findViewById(R.id.btnLocation) as Button
-        btnLocation.setOnClickListener {
-            checkForPermissions(
-                android.Manifest.permission.ACCESS_FINE_LOCATION,
-                "location",
-                FINE_LOCATION_RQ
-            )
-        }
-        val btnNoti = findViewById(R.id.btnNoti) as Button
-        btnNoti.setOnClickListener {
-            sendNotification()
-        }
-        val btnAlertDialog = findViewById(R.id.btnAlertDialog) as Button
-        btnAlertDialog.setOnClickListener {
-            alertDialog()
-        }
-        val btnNotification = findViewById(R.id.btnNotification) as Button
-        btnNotification.setOnClickListener {
-            notification()
+        createNotificationChannel(channelID, "DemoChannel", "this is a demo")
+        val bttn = findViewById(R.id.bttn) as Button
+        bttn.setOnClickListener {
+            displayNotification()
         }
     }
 
-    private fun notification() {
-        @SuppressLint("RemoteViewLayout")
-        val intent = Intent(this, LauncherActivity::class.java)
-        val pendingIntent =
-            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-
-        val contentView = RemoteViews(packageName, R.layout.notification_layout)
-        contentView.setTextViewText(R.id.tv_title, "FeyyyHey")
-        contentView.setTextViewText(R.id.tv_content, "Text heyyofey")
-
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            notificationChannel =
-                NotificationChannel(channelId, descriptor, NotificationManager.IMPORTANCE_HIGH)
-            notificationChannel.enableLights(true)
-            notificationChannel.lightColor = Color.GREEN
-            notificationChannel.enableVibration(false)
-            notificationManager.createNotificationChannel(notificationChannel)
-            builder = NotificationCompat.Builder(this, channelId)
-                //   .setContentTitle("Noti Title f")
-                //   .setContentText("Noti content f")
-                .setContent(contentView)
-                .setSmallIcon(R.mipmap.ic_launcher_round)
-                .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.mipmap.ic_launcher))
-                .setContentIntent(pendingIntent)
-                .setTimeoutAfter(9000)
-        } else {
-            builder = NotificationCompat.Builder(this)
-                //   .setContentTitle("Noti Title f")
-                // .setContentText("Noti content f")
-                .setContent(contentView)
-                .setSmallIcon(R.drawable.ic_launcher_round)
-                .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.ic_launcher))
-                .setContentIntent(pendingIntent)
-                .setTimeoutAfter(9000)
-        }
-        notificationManager.notify(1234, builder.build())
-
-    }
-
-    private fun createNotificationChannel() {
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = "Notification Title"
-            val descriptionText = "Notification Description"
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
-                description = descriptionText
-            }
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
-    }
-
-    private fun alertDialog() {
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle("Permission")
-        builder.setMessage("Do you allow?")
-        builder.setPositiveButton("Yes", { dialogInterface: DialogInterface, i: Int ->
-            finish()
-        })
-        builder.setNegativeButton("No", { dialogInterface: DialogInterface, i: Int -> })
-        builder.show()
-    }
-
-    private fun sendNotification() {
-        createNotificationChannel()
-        val intent = Intent(this, MainActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        }
-        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
-
-        val bitmap = BitmapFactory.decodeResource(
-            applicationContext.resources,
-            R.drawable.ic_launcher_foreground
-        )
-        val bitmapLargeIcon = BitmapFactory.decodeResource(
-            applicationContext.resources,
-            R.drawable.ic_launcher_foreground
+    private fun displayNotification() {
+        val notificationId = 45
+        val tapResultIntent = Intent(this, SecondActivity::class.java)
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            tapResultIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
         )
 
+        //reply action
+        val remoteInput:RemoteInput = RemoteInput.Builder(KEY_REPLY).run{
+            setLabel("Insert you name here")
+            build()
+        }
 
-        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_launcher_background)
-            .setContentTitle("my notifi")
-            .setContentText("my fejges fey ")
-            .setLargeIcon(bitmapLargeIcon)
-            .setStyle(NotificationCompat.BigTextStyle().bigText("much longer text that lds"))
-            // .setStyle(NotificationCompat.BigPictureStyle().bigPicture(bitmap))
-            .setContentIntent(pendingIntent)
+        val replyAction : NotificationCompat.Action = NotificationCompat.Action.Builder(
+            0,
+            "REPLY",
+            pendingIntent
+        ).addRemoteInput(remoteInput)
+            .build()
+
+        //action button 1
+        val intent2 = Intent(this, DetailsActivity::class.java)
+        val pendingIntent2: PendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent2,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        val action2 : NotificationCompat.Action =
+            NotificationCompat.Action.Builder(0,"Details",pendingIntent2).build()
+        // action button 2
+        val intent3 = Intent(this, SettingsActivity::class.java)
+        val pendingIntent3: PendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent3,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        val action3 : NotificationCompat.Action =
+            NotificationCompat.Action.Builder(0,"Settings",pendingIntent3).build()
+
+
+
+        val notification = NotificationCompat.Builder(this@MainActivity, channelID)
+            .setContentTitle("Demo Title")
+            .setContentText("This is a demo notification")
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .setAutoCancel(true)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .addAction(action2)
+            .addAction(action3)
+            .addAction(replyAction)
+            .build()
+        notificationManager?.notify(notificationId, notification)
 
-        with(NotificationManagerCompat.from(this)) {
-            notify(notificationId, builder.build())
-        }
     }
 
-    private fun checkForPermissions(permission: String, name: String, requestCode: Int) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            when {
-                ContextCompat.checkSelfPermission(
-                    applicationContext,
-                    permission
-                ) == PackageManager.PERMISSION_GRANTED -> {
-                    Toast.makeText(
-                        applicationContext,
-                        "$name permission granted",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-                shouldShowRequestPermissionRationale(permission) -> showDialog(
-                    permission,
-                    name,
-                    requestCode
-                )
-                else -> ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
+    private fun createNotificationChannel(id: String, name: String, channelDescription: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(id, name, importance).apply {
+                description = channelDescription
             }
+            notificationManager?.createNotificationChannel(channel)
 
         }
 
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        fun innerCheck(name: String) {
-            if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(applicationContext, "$name permission refused", Toast.LENGTH_SHORT)
-                    .show()
-            } else {
-                Toast.makeText(applicationContext, "$name permission granted", Toast.LENGTH_SHORT)
-                    .show()
-            }
-        }
-        when (requestCode) {
-            FINE_LOCATION_RQ -> innerCheck("location")
-            CAMERA_RQ -> innerCheck("camera")
-        }
-    }
-
-    private fun showDialog(permission: String, name: String, requestCode: Int) {
-        val builder = AlertDialog.Builder(this)
-        builder.apply {
-            setMessage("Permission to access your $name is requred to use this app")
-            setTitle("Permission required")
-            setPositiveButton("OK") { dialog, which ->
-                ActivityCompat.requestPermissions(
-                    this@MainActivity,
-                    arrayOf(permission),
-                    requestCode
-                )
-            }
-        }
-        val dialog = builder.create()
-        dialog.show()
-    }
 }
